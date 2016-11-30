bec13808f2839efc7e6d39556e499c3f154a06a3
rainmelody <croissantcronicle@gmail.com>
2016-11-30 10:48:36 +0700

    nambah header
    buat master karyawan + promosi

--- application/views/HeaderMaster.php
+++ application/views/HeaderMaster.php
@@ -1,9 +1,12 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bs/css/bootstrap.css')?>"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bs/css/jquery.dataTables.min.css')?>"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/bs/css/bootstrap-datetimepicker.min.css')?>"/>
 	<script src="<?php echo base_url('assets/bs/js/jquery-3.1.1.min.js')?>"></script>
 	<script src="<?php echo base_url('assets/bs/js/bootstrap.js')?>"></script>
 	<script src="<?php echo base_url('assets/bs/js/jquery.dataTables.min.js')?>"></script>
+	<script src="<?php echo base_url('assets/bs/js/bootstrap-datetimepicker.js')?>"></script>
+	<script src="<?php echo base_url('assets/bs/js/locales/bootstrap-datetimepicker.fr.js')?>"></script>
 	<style>
 		.color1
 		{
@@ -19,10 +22,12 @@
 				</div>
 				
 				<ul class="nav navbar-nav">
-					<li><a href="<?php echo site_url("Master/index")?>">Home</a></li>
+					<li><a href="<?php echo site_url("Master/index")?>">Master Produk</a></li>
 					<li><a href="<?php echo site_url("Master/ListProduk")?>">List Produk</a></li>
-					<li><a href="#">List Produk</a></li>
-					<li><a href="#">Page3</a></li>
+					<li><a href="<?php echo site_url("Master_Karyawan/index")?>">Master Karyawan</a></li>
+					<li><a href="<?php echo site_url("Master_Karyawan/ListKaryawan")?>">List Karyawan</a></li>
+					<li><a href="<?php echo site_url("MasterPromosi/index")?>">Master Promosi</a></li>
+					<li><a href="<?php echo site_url("MasterPromosi/ListPromosi")?>">List Promosi</a></li>
 				</ul>
 			</div>
 		</nav>
